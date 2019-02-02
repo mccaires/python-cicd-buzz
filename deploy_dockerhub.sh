307b5af4a20ed5ee79ce87cb3cf9a79ca232a4a6
mccaires <47260226+mccaires@users.noreply.github.com>
2019-02-01 22:03:26 -0200

    Update deploy_dockerhub.sh

--- deploy_dockerhub.sh
+++ deploy_dockerhub.sh
@@ -2,5 +2,5 @@
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 TAG="$TRAVIS_BUILD_NUMBER"
 
-docker build Dockerfile -t devfiap/mccaires:$TAG .
+docker build -t devfiap/mccaires:$TAG .
 docker push devfiap/mccaires
